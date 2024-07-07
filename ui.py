8b08cf4fb5b7c45a3587ce9c7ea13bff001fc2a2
anoushka1111 <saha.anoushka@gmail.com>
2024-07-06 20:56:18 -0400

    starting ui

--- ui.py
+++ ui.py
@@ -1,2 +1,22 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+HEME_COLOR = "#375362"
+
+class QuizUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("True/False Quiz")
+        self.window.minsize(width=700, height=700)
+        self.window.config(bg="#06327E")
+
+        self.score_label = Label(text = "Score: 0", fg = "white", bg = "#06327E")
+        self.score_label.grid(row = 0, column = 1)
+
+        self.canvas = Canvas(width=600, height=600, bg = "white")
+        self.qtext = self.canvas.create_text(300, 300, text = "some question text", fill = "black")
+        self.canvas.grid(row = 1, column = 0, columnspan = 2)
+
+
+        #self.question_bg = PhotoImage(file="images/Question.png")
+        #self.card_image = self.canvas.create_image(300, 300, image = self.question_bg)
+        self.window.mainloop()
