9cadda9024cacc52ccb2297568e3a1a75ffa1b02
anoushka1111 <saha.anoushka@gmail.com>
2024-07-13 23:42:31 -0400

    ui changes/testing

--- ui.py
+++ ui.py
@@ -9,12 +9,12 @@ class QuizUI:
         self.window.minsize(width=700, height=700)
         self.window.config(bg="#06327E")
 
-        self.score_label = Label(text = "Score: 0", fg = "white", bg = "#06327E")
-        self.score_label.grid(row = 0, column = 1)
+        #self.score_text = Label(text = "Score: 0", fg = "white", bg = "#06327E")
+        #self.score_label.grid(row = 0, column = 1)
 
-        self.canvas = Canvas(width=600, height=600, bg = "white")
+        self.canvas = Canvas(self.window, width=600, height=600, bg = "white")
         self.qtext = self.canvas.create_text(300, 300, text = "some question text", fill = "black")
-        self.canvas.grid(row = 1, column = 0, columnspan = 2)
+        #self.canvas.grid(row = 1, column = 0, columnspan = 2)
 
 
         #self.question_bg = PhotoImage(file="images/Question.png")
